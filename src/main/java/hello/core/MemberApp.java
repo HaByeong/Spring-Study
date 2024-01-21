d83249c9c1891d84fc5676141439ddcb8d15ea8a
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-21 09:47:28 +0900

    관심사의 분리

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,9 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfing = new AppConfig();
+        MemberService memberService = appConfing.memberService();
+       // MemberService memberService = new MemberServiceImpl();
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -27,10 +27,10 @@ class RateDiscountPolicyTest {
     @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
     void vip_x() {
         //given
-        Member member = new Member(2L, "memberA", Grade.BASIC);
+        Member member = new Member(2L, "memberB", Grade.BASIC);
         //when
         int discount = discountPolicy.discount(member, 10000);
         //then
-        assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(0);
     }
 }
