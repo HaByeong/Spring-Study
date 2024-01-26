727d24a06036f8ee048bdd18fbb665d4a43b60bf
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-26 16:06:29 +0900

    주입할 스프링 없을 시 처리

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,36 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        // 스프링 빈이 아닌 즉, 스프링 컨테이너에서 관리되지 않는 객체를 가지고 테스트중
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+
+    }
+}
