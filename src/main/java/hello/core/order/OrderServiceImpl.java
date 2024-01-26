19fa6b56022bb41b5145fb714dbd3cccdd50bde0
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-01-26 17:09:44 +0900

    Lombok 사용하여 코드 간결하게 만들기

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,12 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -17,13 +19,14 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
     // 이렇게 되면 Order~Impl이 Fix~Policy와 Rate~Policy를 의존한다. -> DiscountPolicy 즉, 추상뿐만 아니라 구현 클래스에도 의존
 
-    @Autowired // 생성자 개수가 하나일 경우에는 생략해도 된다.
+    /*@Autowired -> 생성자 개수가 하나일 경우에는 생략해도 된다.
+    @RequiredArgsConstructor가 final이 붙은 필수값들에 대한 생성자를 자동으로 만들어준다~~
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    */
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
