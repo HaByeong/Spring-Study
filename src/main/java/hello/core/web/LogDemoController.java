ebc981a87913fdefa9c1821dc97dd69bcf3ee118
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-02-01 10:04:51 +0900

    스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,13 +13,15 @@ import hello.core.common.MyLogger;
 public class  LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
+    //HttpServletRequest -> 자바에서 제공하는 표준 서블릿 규약이 있는데 그에 의한 HttpRequest 정보를 받을 수 있다.
     @RequestMapping("log-demo")
     @ResponseBody
-    //HttpServletRequest -> 자바에서 제공하는 표준 서블릿 규약이 있는데 그에 의한 HttpRequest 정보를 받을 수 있다.
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        //logDemo에서 HTTP 요청이 들어온다. -> 아래 getObject()로 해당 빈을 요청하여 가져올 수 있음!
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
